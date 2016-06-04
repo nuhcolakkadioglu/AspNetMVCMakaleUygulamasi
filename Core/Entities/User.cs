ae139fe71b453751aba4b0d565521c4c1c52d512
Nuh <hex-tr@hotmail.com>
2016-06-04 16:16:59 +0300

    entity içinde base ve user sınıfları oluşturuldu

---
+++ Core/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public partial class User : Base
+    {
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Job { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
